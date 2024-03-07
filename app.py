7c5483702963397c1a502deaeead8e9b8df4bf06
tejko23 <mateuszswiatek@yahoo.com>
2024-03-07 15:02:44 +0100

    create app.py

---
+++ app.py
@@ -0,0 +1,25 @@
+import customtkinter
+
+class App(customtkinter.CTk):
+    def __init__(self):
+        super().__init__()
+
+        self.title("Pomodoro")
+        self.geometry("400x200")
+        self.grid_columnconfigure((0, 1), weight=1)
+
+        self.label = customtkinter.CTkLabel(self, text="Pomodoro", 
+                                            font=customtkinter.CTkFont(
+                                                size=50
+                                                ))
+        self.label.grid(row=0, 
+                        column=0, 
+                        padx=20, 
+                        pady=20, 
+                        sticky='ew', 
+                        columnspan=2)
+        
+
+if __name__ == '__main__':
+    app = App()
+    app.mainloop()
