ee6ee1b6717961421854b6a4716bfd340cfd866e
tejko23 <mateuszswiatek@yahoo.com>
2024-03-15 12:40:51 +0100

    refactor Timer to use only timedelta

--- timer.py
+++ timer.py
@@ -3,7 +3,6 @@ from datetime import datetime, timedelta
 class Timer:
     def __init__(self, time: str):
         self._time = None
-        self._delta = None
         self.time = time 
 
     def __str__(self) -> str:
@@ -11,26 +10,20 @@ class Timer:
 
     @property 
     def time(self) -> str: 
-        return datetime.strftime(self._time, "%M:%S")
+        mm, ss = divmod(self._time.seconds, 60)
+        return f"{mm:02}:{ss:02}"
 
     @time.setter 
     def time(self, time_value: str) -> None: 
         if not isinstance(time_value, str):
             raise ValueError(f"{time_value} must be str.") 
-        self._time = datetime.strptime(time_value, "%M")
-        self._update_delta()
-        
-    def _update_delta(self) -> None:
-        self._delta = timedelta(hours=self._time.hour,
-                                minutes=self._time.minute,
-                                seconds=self._time.second)
+        self._time = timedelta(minutes=int(time_value))
         
     def process(self):
-        if self._delta > timedelta.resolution:
+        if self._time > timedelta.resolution:
             self._time -= timedelta(seconds=1)
-            self._update_delta()
 
     def is_finished(self) -> bool:
-        if self._delta == timedelta(seconds=0):
+        if self._time == timedelta(seconds=0):
             return True
         return False
